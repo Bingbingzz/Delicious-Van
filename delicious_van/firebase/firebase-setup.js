928e999ef09af2953156d8b01d975c4732440a9a
AnnabelleAB <sunhuaqing01@gmail.com>
2023-03-15 11:53:23 -0700

    firestore setup

--- delicious_van/firebase/firebase-setup.js
+++ delicious_van/firebase/firebase-setup.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from '@env';
+import { getFirestore } from 'firebase/firestore';
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,4 +20,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const myApp = initializeApp(firebaseConfig);
+export const firestore = getFirestore(myApp);
+
+export const auth = initializeAuth(myApp, { persistence: getReactNativePersistence(AsyncStorage) })
