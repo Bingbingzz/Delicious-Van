6ab0b7419a6d615c66e267266dad35382282babc
AnnabelleAB <sunhuaqing01@gmail.com>
2023-03-15 11:50:52 -0700

    Initial color pallete

---
+++ delicious_van/colors.js
@@ -0,0 +1,12 @@
+const colors = {
+    primary: '#F5793B',
+    tab: '#f79a6b',
+    secondary:'#f1580c',
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: '#d4d4d4',   
+    border: '#DDD',
+    pressedButtonStyle: "#eeeeee",
+};
+
+export default colors;
