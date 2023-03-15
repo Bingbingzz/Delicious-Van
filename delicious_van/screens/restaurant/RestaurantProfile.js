e0f9d9d878b962e1e860731dfe26c0fb5b2db227
AnnabelleAB <sunhuaqing01@gmail.com>
2023-03-15 14:33:44 -0700

    Create Screens and install UI library

---
+++ delicious_van/screens/restaurant/RestaurantProfile.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function RestaurantProfile() {
+  return (
+    <View>
+      <Text>RestaurantProfile</Text>
+    </View>
+  )
+}
