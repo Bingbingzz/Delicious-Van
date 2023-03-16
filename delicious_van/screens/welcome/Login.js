b9fe83df2d96fbc5a566b375382f3839437705ef
AnnabelleAB <sunhuaqing01@gmail.com>
2023-03-16 13:08:22 -0700

    initiate version of authentication setup

--- delicious_van/screens/welcome/Login.js
+++ delicious_van/screens/welcome/Login.js
@@ -1,10 +1,74 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button, TextInput, StyleSheet, ScrollView } from 'react-native'
+import React, { useState } from 'react'
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from 'delicious_van/firebase/firebase-setup.js'
 
-export default function Login() {
+export default function Login({ navigation }) {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+  const loginHandler = async () => {
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log("signed in", user)
+
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage)
+      });
+  };
+  const signUpHandler = () => {
+    navigation.replace("SignUp")
+  };
   return (
-    <View>
-      <Text>Login</Text>
-    </View>
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.label}>Email</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={(newEmail) => { setEmail(newEmail) }}
+      />
+      <Text style={styles.label}>Password</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(newPassword) => { setPassword(newPassword) }}
+      />
+      <Button title='Login' onPress={loginHandler} />
+      <Button title='Sign Up' onPress={signUpHandler} />
+    </ScrollView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginBottom: 10,
+  },
+}); 
--- delicious_van/screens/welcome/SignUp.js
+++ delicious_van/screens/welcome/SignUp.js
@@ -1,10 +1,87 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button, TextInput, StyleSheet, Alert } from 'react-native'
+import React, { useState } from 'react'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from 'delicious_van/firebase/firebase-setup.js'
 
-export default function SignUp() {
+export default function SignUp({ navigation }) {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [confirmPassword, setConfirmPassword] = useState("")
+
+  const loginHandler = () => {
+    navigation.replace("Login")
+  };
+
+  const signUpHandler = async () => {
+    if (password !== confirmPassword) { Alert.alert("The passwords don't match") }
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        console.log("signed up", user)
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage)
+        // ..
+      });
+  };
+
+
   return (
-    <View>
-      <Text>SignUp</Text>
+    <View style={styles.container}>
+      <Text style={styles.label}>Email</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={(newEmail) => { setEmail(newEmail) }}
+      />
+      <Text style={styles.label}>Password</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        value={password}
+        onChangeText={(newPassword) => { setPassword(newPassword) }}
+      />
+      <Text style={styles.label}>Confirm Password</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Confirm Password"
+        secureTextEntry={true}
+        value={confirmPassword}
+        onChangeText={(newPassword) => { setConfirmPassword(newPassword) }}
+      />
+      <Button title='Register' onPress={signUpHandler} />
+      <Button title='Already registered? Login' onPress={loginHandler} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    padding: 20,
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 10,
+    marginBottom: 5,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginBottom: 10,
+  },
+});
