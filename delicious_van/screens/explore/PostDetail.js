c16cc43e0ff5e6cdde037cc06ac236a6325e32df
alynxu <alynnxu@gmail.com>
2023-03-29 21:21:59 -0700

    PostDetail screen layout updates

--- delicious_van/screens/explore/PostDetail.js
+++ delicious_van/screens/explore/PostDetail.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet, ScrollView, ActionSheetIOS } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView, ActionSheetIOS, Dimensions, TextInput } from 'react-native';
 import PressableButton from '../../components/PressableButton';
 import { useNavigation } from '@react-navigation/native';
-import { Ionicons } from '@expo/vector-icons';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import { Menu } from 'react-native-paper';
 import { deletePostFromDB, getPostFromDB } from '../../firebase/firestoreHelper';
+import { auth } from '../../firebase/firebase-setup';
+import colors from "../../colors";
+import Avatar from "../../assets/avatar.png";
 
 const defaultImage = 'https://i.ibb.co/JtS24qP/default-image.jpg';
+
+const windowHeight = Dimensions.get('window').height;
 
 export default function PostDetail({ route }) {
   const { post } = route.params;
@@ -17,6 +22,7 @@ export default function PostDetail({ route }) {
   const displayImage = (imageUrls && imageUrls[0]) || defaultImage;
   const fetchPostData = async () => {
     const updatedPost = await getPostFromDB(id);
+    console.log(updatedPost)
     if (updatedPost) {
       setPostData(updatedPost);
     }
@@ -40,7 +46,7 @@ export default function PostDetail({ route }) {
               customizedStyle={styles.button}
               buttonPressed={handleMenuPress}
             >
-              <Ionicons name="menu-sharp" size={24} color="white" />
+              <Icon name="more-horiz" size={24} color="white" />
             </PressableButton>
           }
         >
@@ -69,36 +75,94 @@ export default function PostDetail({ route }) {
   };
 
   return (
-    <ScrollView>
+    <ScrollView style={styles.scrollView}>
       <View style={styles.container}>
+        <View style={styles.top}>
+          <Image source={Avatar} style={styles.avatar} />
+          <Text style={styles.email}>{auth.currentUser.email}</Text>
+        </View>
         <Image source={{ uri: displayImage }} style={styles.image} />
         <Text style={styles.title}>{title}</Text>
         <Text style={styles.description}>{description}</Text>
+        <View style={styles.bottom}>
+          <View style={styles.inputWrapper}>
+            <TextInput
+              style={styles.input}
+              placeholder="Say something here"
+            />
+          </View>
+          <View
+            customizedStyle={styles.button}
+          >
+            <Icon name="favorite-border" size={24} />
+          </View>
+        </View>
       </View>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
+  scrollView: {
+    minHeight: windowHeight,
+    backgroundColor: colors.pageContentBgColor,
+  },
+
   container: {
     flex: 1,
-    backgroundColor: 'white',
+    backgroundColor: colors.pageContentBgColor,
     shadowColor: "#000",
+    padding: 16,
   },
+
+  top: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginBottom: 5,
+  },
+
   image: {
     width: '100%',
-    height: 250,
+    height: windowHeight * 0.5,
     resizeMode: 'cover',
+    borderRadius: 12,
   },
+
+  avatar: {
+    width: 50,
+    height: 50,
+    marginRight: 5
+  },
+
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    padding: 16,
+    // padding: 16,
   },
+
+  email: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
   description: {
     fontSize: 16,
-    padding: 16,
-    paddingBottom: 32,
+    // padding: 16,
+    paddingBottom: 20,
     lineHeight: 24,
+    marginTop: 6,
   },
+
+  bottom: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+
+  inputWrapper: {
+    backgroundColor: "#f1f1f1",
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 12
+  }
 });
