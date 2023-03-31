7b959f352797308b0445f1721ad9fe9e1adaa555
alynxu <alynnxu@gmail.com>
2023-03-30 18:00:40 -0700

    PostAdd screen updates with ImagePickManager

--- delicious_van/screens/explore/PostAdd.js
+++ delicious_van/screens/explore/PostAdd.js
@@ -1,19 +1,9 @@
-import {
-  View,
-  TextInput,
-  StyleSheet,
-  Text,
-  Alert,
-  Image,
-  TouchableOpacity,
-} from "react-native";
+import { View, TextInput, StyleSheet, Text, Alert } from "react-native";
 import React, { useState } from "react";
 import colors from "../../colors";
 import PressableButton from "../../components/PressableButton";
 import { writePostToDB } from "../../firebase/firestoreHelper";
-import * as ImagePicker from "expo-image-picker";
-import { FlatList } from "react-native";
-import Icon from "react-native-vector-icons/MaterialIcons";
+import ImagePickManager from "../../components/ImagePickManager";
 
 export default function PostAdd({ navigation }) {
   const [title, setTitle] = useState("");
@@ -21,41 +11,9 @@ export default function PostAdd({ navigation }) {
   const [isValid, setIsValid] = useState(true);
   const [images, setImages] = useState([]);
 
-  const handleImageSelect = async () => {
-    const permissionResult =
-      await ImagePicker.requestMediaLibraryPermissionsAsync();
-
-    if (permissionResult.granted === false) {
-      alert("Permission to access the camera roll is required!");
-      return;
-    }
-
-    const result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
-    });
-
-    if (!result.canceled) {
-      setImages((prevImages) => [...prevImages, result.assets[0].uri]);
-    }
-  };
-
   const handleImageDelete = (index) => {
     setImages((prevImages) => prevImages.filter((_, i) => i !== index));
   };
-  const ImagePreview = ({ uri, onDelete }) => (
-    <View style={styles.imagePreviewContainer}>
-      <Image source={{ uri }} style={styles.image} resizeMode="cover" />
-      <PressableButton
-        customizedStyle={styles.deleteIcon}
-        buttonPressed={onDelete}
-      >
-        <Text style={styles.deleteIconText}>x</Text>
-      </PressableButton>
-    </View>
-  );
 
   const handleSubmit = async () => {
     if (!title || !description) {
@@ -74,7 +32,7 @@ export default function PostAdd({ navigation }) {
         images: images,
       });
     } catch (error) {
-      console.log(error.message);
+      // console.log(error.message);
     }
     navigation.goBack();
   };
@@ -126,36 +84,11 @@ export default function PostAdd({ navigation }) {
           Please enter a valid description
         </Text>
       )}
-      <View style={styles.photosContainer}>
-        <Text style={styles.photosTitle}>Add photos</Text>
-        <View style={styles.photosListWrapper}>
-          {images.length > 0 && (
-            <FlatList
-              style={styles.photosList}
-              data={images}
-              renderItem={({ item, index }) => (
-                <ImagePreview
-                  uri={item}
-                  onDelete={() => handleImageDelete(index)}
-                />
-              )}
-              keyExtractor={(item, index) => index.toString()}
-              horizontal
-            />
-          )}
-          <TouchableOpacity onPress={handleImageSelect}>
-            <View style={styles.uploadBtn}>
-              <Icon name="add" size={20} color={colors.gray} />
-            </View>
-          </TouchableOpacity>
-        </View>
-        {/*<PressableButton*/}
-        {/*    buttonPressed={handleReset}*/}
-        {/*    customizedStyle={[styles.button]}*/}
-        {/*>*/}
-        {/*    <Text style={styles.buttonText}>Reset</Text>*/}
-        {/*</PressableButton>*/}
-      </View>
+      <ImagePickManager
+        images={images}
+        setImages={setImages}
+        handleImageDelete={handleImageDelete}
+      />
       <View style={styles.bottomContainer}>
         <PressableButton
           buttonPressed={handleSubmit}
@@ -213,42 +146,6 @@ const styles = StyleSheet.create({
     width: "100%",
   },
 
-  photosContainer: {
-    // flexDirection: 'row',
-    justifyContent: "flex-start",
-    marginTop: 16,
-    width: "100%",
-    flex: 1,
-    alignItems: "flex-start",
-  },
-
-  photosTitle: {
-    fontSize: 18,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-
-  photosListWrapper: {
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    alignItems: "center",
-  },
-
-  photosList: {
-    flexGrow: 0,
-  },
-
-  uploadBtn: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: 80,
-    height: 80,
-    borderWidth: 1,
-    borderStyle: "solid",
-    borderColor: colors.gray,
-    borderRadius: 10,
-  },
-
   button: {
     backgroundColor: colors.buttonBackground,
     borderRadius: 5,
@@ -269,35 +166,6 @@ const styles = StyleSheet.create({
   errorMessage: {
     color: "red",
     fontSize: 12,
-  },
-
-  imagePreviewContainer: {
-    position: "relative",
-    marginRight: 10,
-  },
-
-  image: {
-    width: 80,
-    height: 80,
-    borderRadius: 5,
-  },
-
-  deleteIcon: {
-    position: "absolute",
-    top: -5,
-    right: -5,
-    backgroundColor: "red",
-    borderRadius: 10,
-    width: 20,
-    height: 20,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-
-  deleteIconText: {
-    color: "white",
-    fontSize: 14,
-    fontWeight: "bold",
   },
 
   bottomContainer: {
