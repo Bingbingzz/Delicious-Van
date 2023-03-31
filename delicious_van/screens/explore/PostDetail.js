5e32c97a333353ab6994e5ee6b5a3bc0762f4b6e
alynxu <alynnxu@gmail.com>
2023-03-30 20:13:48 -0700

    comment&favorite feature added on PostDetail screen

--- delicious_van/screens/explore/PostDetail.js
+++ delicious_van/screens/explore/PostDetail.js
@@ -1,34 +1,52 @@
-import React, { useState } from 'react';
-import { View, Text, Image, StyleSheet, ScrollView, ActionSheetIOS, Dimensions, TextInput, Alert } from 'react-native';
-import PressableButton from '../../components/PressableButton';
-import { useNavigation } from '@react-navigation/native';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-import { Menu } from 'react-native-paper';
-import { deletePostFromDB, getPostFromDB } from '../../firebase/firestoreHelper';
-import { auth } from '../../firebase/firebase-setup';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  ActionSheetIOS,
+  Dimensions,
+  TextInput,
+  Alert,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
+import PressableButton from "../../components/PressableButton";
+import { useNavigation } from "@react-navigation/native";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import { Menu } from "react-native-paper";
+import {
+  deletePostFromDB,
+  getPostFromDB,
+  updatePostInDB,
+} from "../../firebase/firestoreHelper";
+import { auth } from "../../firebase/firebase-setup";
 import colors from "../../colors";
 import Avatar from "../../assets/avatar.png";
 
-const defaultImage = 'https://i.ibb.co/JtS24qP/default-image.jpg';
+const defaultImage = "https://i.ibb.co/JtS24qP/default-image.jpg";
 
-const windowHeight = Dimensions.get('window').height;
+const windowHeight = Dimensions.get("window").height;
+const windowWidth = Dimensions.get("window").width;
 
 export default function PostDetail({ route }) {
   const { post } = route.params;
   const navigation = useNavigation();
-  const [menuVisible, setMenuVisible] = useState(false)
+  const [menuVisible, setMenuVisible] = useState(false);
+  const [comment, setComment] = useState("");
   const [postData, setPostData] = useState(post);
   const { title, imageUrls, description, id } = postData;
   const displayImage = (imageUrls && imageUrls[0]) || defaultImage;
   const fetchPostData = async () => {
     const updatedPost = await getPostFromDB(id);
-    console.log(updatedPost)
+    console.log(updatedPost);
     if (updatedPost) {
       setPostData(updatedPost);
     }
   };
   React.useEffect(() => {
-    const unsubscribe = navigation.addListener('focus', () => {
+    const unsubscribe = navigation.addListener("focus", () => {
       fetchPostData();
     });
 
@@ -57,33 +75,93 @@ export default function PostDetail({ route }) {
     });
   }, [navigation, menuVisible]);
 
-
-
   const handleMenuPress = () => {
     setMenuVisible(!menuVisible);
   };
 
   const handleDeleteItem = () => {
     setMenuVisible(false);
-    Alert.alert('Do you want to delete this post?', '', [
+    Alert.alert("Do you want to delete this post?", "", [
       {
-        text: 'Cancel',
-        onPress: () => console.log('Cancel Pressed'),
-        style: 'cancel',
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel",
       },
       {
-        text: 'OK',
+        text: "OK",
         onPress: () => {
           deletePostFromDB(id);
           navigation.goBack();
-        }
+        },
       },
     ]);
   };
 
   const handleEditItem = () => {
     setMenuVisible(false);
-    navigation.navigate('PostEdit', { post });
+    navigation.navigate("PostEdit", { post });
+  };
+
+  const sendComment = () => {
+    // create new comment
+    const newComment = {
+      userEmail: auth.currentUser.email,
+      userId: auth.currentUser.uid,
+      content: comment,
+      date: Date.now(),
+    };
+    // add the new comment to post comment list
+    if (postData.comments) {
+      postData.comments.push(newComment);
+    } else {
+      postData.comments = [newComment];
+    }
+    // update post
+    updatePostInDB(id, postData).then(() => {
+      setPostData({ ...postData });
+      // reset comment input
+      setComment("");
+    });
+  };
+
+  const likeComment = () => {
+    if (postData.likes && postData.likes.includes(auth.currentUser.uid)) {
+      // remove user from post like list
+      postData.likes = postData.likes.filter(
+        (item) => item !== auth.currentUser.uid
+      );
+    } else {
+      // add user to post like list
+      if (postData.likes) {
+        postData.likes.push(auth.currentUser.uid);
+      } else {
+        postData.likes = [auth.currentUser.uid];
+      }
+    }
+    // update post
+    updatePostInDB(id, postData).then(() => {
+      setPostData({ ...postData });
+    });
+  };
+
+  const deleteComment = (index) => {
+    Alert.alert("Do you want to delete this comment?", "", [
+      {
+        text: "Cancel",
+        onPress: () => console.log("Cancel Pressed"),
+        style: "cancel",
+      },
+      {
+        text: "OK",
+        onPress: () => {
+          postData.comments.splice(index);
+          // update post
+          updatePostInDB(id, postData).then(() => {
+            setPostData({ ...postData });
+          });
+        },
+      },
+    ]);
   };
 
   return (
@@ -100,14 +178,52 @@ export default function PostDetail({ route }) {
           <View style={styles.inputWrapper}>
             <TextInput
               style={styles.input}
+              value={comment}
               placeholder="Say something here"
+              onChangeText={(newComment) => {
+                setComment(newComment);
+              }}
             />
+            <TouchableOpacity onPress={sendComment}>
+              <Icon name="send" size={24} color="#b1b1b1" />
+            </TouchableOpacity>
           </View>
-          <View
-            customizedStyle={styles.button}
-          >
-            <Icon name="favorite-border" size={24} />
+          <View customizedStyle={styles.button}>
+            <TouchableOpacity onPress={likeComment}>
+              <View style={styles.likeWrapper}>
+                {postData.likes &&
+                postData.likes.includes(auth.currentUser.uid) ? (
+                  <Icon name="favorite" size={24} />
+                ) : (
+                  <Icon name="favorite-border" size={24} />
+                )}
+                {postData.likes && postData.likes.length > 0 && (
+                  <Text style={styles.badge}>{postData.likes.length}</Text>
+                )}
+              </View>
+            </TouchableOpacity>
           </View>
+        </View>
+        <View style={styles.commentsList}>
+          {postData.comments &&
+            postData.comments.map((comment, index) => (
+              <View key={index} style={styles.commentItem}>
+                <View>
+                  <Text>{comment.userEmail}</Text>
+                  <Text style={styles.commentDate}>
+                    {new Date(comment.date).toLocaleString()}
+                  </Text>
+                </View>
+                <View>
+                  <Text>{comment.content}</Text>
+                </View>
+                <View style={styles.commentActions}>
+                  <TouchableOpacity onPress={() => deleteComment(index)}>
+                    <Icon name="close" size={18} color="#c75450" />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ))}
         </View>
       </View>
     </ScrollView>
@@ -116,7 +232,7 @@ export default function PostDetail({ route }) {
 
 const styles = StyleSheet.create({
   scrollView: {
-    minHeight: windowHeight,
+    // minHeight: windowHeight,
     backgroundColor: colors.pageContentBgColor,
   },
 
@@ -134,27 +250,27 @@ const styles = StyleSheet.create({
   },
 
   image: {
-    width: '100%',
+    width: "100%",
     height: windowHeight * 0.5,
-    resizeMode: 'cover',
+    resizeMode: "cover",
     borderRadius: 12,
   },
 
   avatar: {
     width: 50,
     height: 50,
-    marginRight: 5
+    marginRight: 5,
   },
 
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     // padding: 16,
   },
 
   email: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
 
   description: {
@@ -168,13 +284,66 @@ const styles = StyleSheet.create({
   bottom: {
     flexDirection: "row",
     justifyContent: "space-between",
-    alignItems: "center"
+    alignItems: "center",
   },
 
   inputWrapper: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     backgroundColor: "#f1f1f1",
     paddingVertical: 5,
     paddingHorizontal: 10,
-    borderRadius: 12
-  }
+    borderRadius: 12,
+  },
+
+  input: {
+    width: windowWidth / 2,
+  },
+
+  commentsList: {
+    marginVertical: 20,
+  },
+
+  commentItem: {
+    position: "relative",
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+    borderBottomWidth: 1,
+    borderStyle: "solid",
+    borderColor: "#e1e1e1",
+    marginBottom: 10,
+  },
+
+  commentDate: {
+    fontSize: 13,
+    color: "#b1b1b1",
+    marginTop: 4,
+    marginBottom: 5,
+  },
+
+  likeWrapper: {
+    position: "relative",
+  },
+
+  badge: {
+    width: 15,
+    height: 15,
+    backgroundColor: "red",
+    color: "white",
+    textAlign: "center",
+    lineHeight: 15,
+    borderRadius: 15,
+    position: "absolute",
+    right: -5,
+    top: -5,
+    fontSize: 12,
+  },
+
+  commentActions: {
+    position: "absolute",
+    top: 5,
+    right: 5,
+  },
 });
