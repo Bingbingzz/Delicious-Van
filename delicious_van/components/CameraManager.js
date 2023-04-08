9c54b4bb680b14f2a98427b6830724c27618c470
alynxu <alynnxu@gmail.com>
2023-04-08 05:19:49 -0700

    Camera-Access Permission bug fixed

--- delicious_van/components/CameraManager.js
+++ delicious_van/components/CameraManager.js
@@ -1,16 +1,8 @@
 import React, { useEffect, useRef } from "react";
 import { Camera, CameraType } from "expo-camera";
-import {
-  Dimensions,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from "react-native";
-import PressableButton from "./PressableButton";
-import { Entypo } from "@expo/vector-icons";
+import {Dimensions, StyleSheet} from "react-native";
 import { useRoute } from "@react-navigation/native";
-import { IconButton, MD3Colors } from "react-native-paper";
+import {IconButton} from "react-native-paper";
 
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
@@ -21,6 +13,10 @@ function CameraManager({ navigation }) {
 
   const [permission, requestPermission] = Camera.useCameraPermissions();
 
+  useEffect(() => {
+    requestPermission();
+  }, [])
+
   const takePicture = async () => {
     if (!camera) return;
     const photo = await camera.takePictureAsync();
@@ -29,22 +25,27 @@ function CameraManager({ navigation }) {
   };
 
   return (
-    <Camera
-      ref={(r) => {
-        camera = r;
-      }}
-      type={CameraType.back}
-      style={styles.camera}
-    >
-      <IconButton
-        icon="camera"
-        iconColor="white"
-        size={50}
-        style={styles.takePhotoBtn}
-        onPress={takePicture}
-      />
-    </Camera>
-  );
+    <>
+      {
+        permission && permission.granted && (
+          <Camera
+            ref={(r) => {
+              camera = r
+            }}
+            type={CameraType.back}
+            style={styles.camera}>
+            <IconButton
+              icon="camera"
+              iconColor="white"
+              size={50}
+              style={styles.takePhotoBtn}
+              onPress={takePicture}
+            />
+          </Camera>
+        )
+      }
+    </>
+  )
 }
 
 export default CameraManager;
