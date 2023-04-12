f306a5a560b36856823471ba7d2470feee631353
alynxu <alynnxu@gmail.com>
2023-04-12 00:29:11 -0700

    UserProfile screen updated with selections

--- delicious_van/screens/me/UserProfile.js
+++ delicious_van/screens/me/UserProfile.js
@@ -1,10 +1,16 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import PressableButton from "../../components/PressableButton";
 import { auth } from "../../firebase/firebase-setup";
 import colors from "../../colors";
+import Avatar from "../../assets/avatar.png";
+import { IconButton } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
 import NotificaitonManager from "../../components/NotificaitonManager";
+
 export default function UserProfile() {
+  const navigation = useNavigation();
+
   const LogoutHandler = async () => {
     try {
       await auth.signOut();
@@ -17,14 +23,36 @@ export default function UserProfile() {
 
   return (
     <View style={styles.container}>
-      <Text>{auth.currentUser.email}</Text>
-      <Text>{auth.currentUser.uid}</Text>
-      <NotificaitonManager/>
+      <View style={styles.top}>
+        <Image source={Avatar} style={styles.avatar} />
+        <Text style={styles.email}>{auth.currentUser.email}</Text>
+      </View>
+      <View style={styles.center}>
+        <TouchableOpacity onPress={() => navigation.navigate("ProfileEdit")}>
+          <View style={styles.item}>
+            <Text style={styles.itemText}>Profile</Text>
+            <IconButton icon="chevron-right" iconColor="#c1c1c1" />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => console.log(222)}>
+          <View style={styles.item}>
+            <Text style={styles.itemText}>Comments</Text>
+            <IconButton icon="chevron-right" iconColor="#c1c1c1" />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => console.log(333)}>
+          <View style={styles.item}>
+            <Text style={styles.itemText}>Favorites</Text>
+            <IconButton icon="chevron-right" iconColor="#c1c1c1" />
+          </View>
+        </TouchableOpacity>
+      </View>
+      <NotificaitonManager />
       <PressableButton
         customizedStyle={styles.button}
         buttonPressed={LogoutHandler}
       >
-        <Text>Logout</Text>
+        <Text style={styles.btnText}>Logout</Text>
       </PressableButton>
     </View>
   );
@@ -43,4 +71,43 @@ const styles = StyleSheet.create({
     paddingVertical: 8,
     marginTop: 16,
   },
+  btnText: {
+    color: colors.white,
+  },
+  top: {
+    flexDirection: "column",
+    alignItems: "center",
+    marginBottom: 20,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    marginRight: 5,
+  },
+  email: {
+    color: colors.primary,
+    marginTop: 10,
+    fontSize: 18,
+    fontWeight: 'bold'
+  },
+  center: {
+    width: '100%',
+    padding: 40
+  },
+  item: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    paddingHorizontal: 20,
+    paddingVertical: 6,
+    borderStyle: 'solid',
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#c8d6d7',
+    marginBottom: 10,
+  },
+  itemText: {
+    fontWeight: 'bold'
+  }
 });
