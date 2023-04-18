ce7df915dbaaf23608c840d7cba21f4e3dff1162
Lydia <zhao.ce1@northeastern.edu>
2023-04-18 00:09:38 -0700

    add time picker for the schedule notification

--- delicious_van/components/NotificaitonManager.js
+++ delicious_van/components/NotificaitonManager.js
@@ -1,50 +1,89 @@
-import { View, Text, StyleSheet, Button } from "react-native";
-import React from "react";
-import PressableButton from "./PressableButton";
-import colors from "../colors";
+import { View, Button, Alert } from "react-native";
+import React, { useState } from "react";
 import * as Notifications from "expo-notifications";
+import DateTimePicker from '@react-native-community/datetimepicker';
 
-export default function NotificaitonManager() {
-    async function verifyPermission() {
-        const permissionResponse=await Notifications.getPermissionsAsync();
-        console.log(permissionResponse);
+export default function NotificationManager() {
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [chosenDate, setChosenDate] = useState(new Date());
 
-        if (permissionResponse.granted) {
-          return true;
-        }
-        try {
-          const permissionResult = await Notifications.requestPermissionsAsync();
-          return permissionResult.granted;
-        } catch (err) {
-          //console.log("permission request error ", err);
-        }
-      }
-  async function scheduleNotificationHandler() {
-    const hasPermission = await verifyPermission();
-    if(!hasPermission){
-        Alert.alert("You need to give notification permission");
+  async function verifyPermission() {
+    const permissionResponse = await Notifications.getPermissionsAsync();
+    if (permissionResponse.granted) {
+      return true;
     }
-    await Notifications.scheduleNotificationAsync({
-      content: {
-        title: "Find some food:",
-        body: "View more recommendations!",
-      },
-      trigger: {hour: 16, minute:0, repeats: true },
-    });
+    try {
+      const permissionResult = await Notifications.requestPermissionsAsync();
+      return permissionResult.granted;
+    } catch (err) {
+      //console.log("permission request error ", err);
+    }
   }
+
+  async function handleNotificationSchedule() {
+    const hasPermission = await verifyPermission();
+    if (!hasPermission) {
+      Alert.alert("You need to give notification permission");
+    }
+    try {
+      const trigger = new Date();
+      trigger.setHours(chosenDate.getHours());
+      trigger.setMinutes(chosenDate.getMinutes());
+      trigger.setSeconds(0);
+      await Notifications.cancelAllScheduledNotificationsAsync();
+      await Notifications.scheduleNotificationAsync({
+        content: {
+          title: "Find some food:",
+          body: "View more recommendations!",
+        },
+        trigger: {
+          date: trigger,
+          repeats: true,
+        },
+      });
+      Alert.alert("Notification scheduled at " + chosenDate.toLocaleTimeString());
+    } catch (error) {
+      console.log("Error scheduling notification: ", error);
+      Alert.alert("Error scheduling notification");
+    }
+    setShowDatePicker(false);
+  }
+
+  function showDatePickerHandler() {
+    setShowDatePicker(true);
+  }
+
+  function hideDatePickerHandler() {
+    setShowDatePicker(false);
+  }
+
+  function onDateChangeHandler(event, selectedDate) {
+    const currentDate = selectedDate || chosenDate;
+    setChosenDate(currentDate);
+  }
+
+  function cancelNotificationTimeHandler() {
+    setShowDatePicker(false);
+    //setChosenDate(new Date());
+  }
+
   return (
     <View>
-      <Button title="Schedual a notification" onPress={scheduleNotificationHandler}/>
+      <Button title="Schedule a notification" onPress={showDatePickerHandler} />
+      {showDatePicker && (
+        <View>
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={chosenDate}
+            mode="time"
+            is24Hour={true}
+            display="default"
+            onChange={onDateChangeHandler}
+          />
+          <Button title="Confirm" onPress={handleNotificationSchedule} />
+          <Button title="Cancel" onPress={cancelNotificationTimeHandler} />
+        </View>
+      )}
     </View>
   );
 }
-const styles = StyleSheet.create({
-    button: {
-      backgroundColor: colors.primary,
-      borderRadius: 8,
-      paddingHorizontal: 16,
-      paddingVertical: 8,
-      marginTop: 16,
-    },
-  });
-  
