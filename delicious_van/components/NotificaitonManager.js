8105fdc274aab9c06833ca395f01e83d5fc91836
Lydia <zhao.ce1@northeastern.edu>
2023-04-18 11:43:16 -0700

    change the style of the button and time picker

--- delicious_van/components/NotificaitonManager.js
+++ delicious_van/components/NotificaitonManager.js
@@ -1,15 +1,25 @@
-import { View, Button, Alert } from "react-native";
+import { View, Button, Alert, Text } from "react-native";
 import React, { useState } from "react";
 import * as Notifications from "expo-notifications";
-import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePicker from "@react-native-community/datetimepicker";
+import PressableButton from "./PressableButton";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import Modal from "react-native-modal";
+import colors from "../colors";
 
 export default function NotificationManager() {
-  const [showDatePicker, setShowDatePicker] = useState(false);
+  // const [showDatePicker, setShowDatePicker] = useState(false);
   const [chosenDate, setChosenDate] = useState(new Date());
+  const [isModalVisible, setModalVisible] = useState(false);
+
+  const toggleModal = () => {
+    setModalVisible(!isModalVisible);
+  };
 
   async function verifyPermission() {
     const permissionResponse = await Notifications.getPermissionsAsync();
     if (permissionResponse.granted) {
+      console.log("permitted");
       return true;
     }
     try {
@@ -21,15 +31,18 @@ export default function NotificationManager() {
   }
 
   async function handleNotificationSchedule() {
+    console.log("here");
     const hasPermission = await verifyPermission();
     if (!hasPermission) {
       Alert.alert("You need to give notification permission");
     }
     try {
       const trigger = new Date();
+      console.log(trigger);
       trigger.setHours(chosenDate.getHours());
       trigger.setMinutes(chosenDate.getMinutes());
       trigger.setSeconds(0);
+      console.log(trigger);
       await Notifications.cancelAllScheduledNotificationsAsync();
       await Notifications.scheduleNotificationAsync({
         content: {
@@ -41,49 +54,95 @@ export default function NotificationManager() {
           repeats: true,
         },
       });
-      Alert.alert("Notification scheduled at " + chosenDate.toLocaleTimeString());
+      console.log(date);
+      Alert.alert(
+        "Notification scheduled at " + chosenDate.toLocaleTimeString()
+      );
     } catch (error) {
       console.log("Error scheduling notification: ", error);
       Alert.alert("Error scheduling notification");
     }
-    setShowDatePicker(false);
-  }
-
-  function showDatePickerHandler() {
-    setShowDatePicker(true);
-  }
-
-  function hideDatePickerHandler() {
-    setShowDatePicker(false);
+    setModalVisible(false);
   }
 
   function onDateChangeHandler(event, selectedDate) {
     const currentDate = selectedDate || chosenDate;
     setChosenDate(currentDate);
+    console.log(chosenDate);
   }
 
   function cancelNotificationTimeHandler() {
-    setShowDatePicker(false);
+    setModalVisible(false);
     //setChosenDate(new Date());
   }
 
   return (
     <View>
-      <Button title="Schedule a notification" onPress={showDatePickerHandler} />
-      {showDatePicker && (
-        <View>
-          <DateTimePicker
-            testID="dateTimePicker"
-            value={chosenDate}
-            mode="time"
-            is24Hour={true}
-            display="default"
-            onChange={onDateChangeHandler}
-          />
-          <Button title="Confirm" onPress={handleNotificationSchedule} />
-          <Button title="Cancel" onPress={cancelNotificationTimeHandler} />
+      <TouchableOpacity onPress={toggleModal} style={styles.scheduleButton}>
+        <Text style={styles.scheduleButtonText}>Schedule a notification</Text>
+      </TouchableOpacity>
+      <Modal isVisible={isModalVisible} onBackdropPress={toggleModal}>
+        <View style={styles.modalContent}>
+          <View style={styles.dateTimePickerContainer}>
+            <DateTimePicker 
+              testID="dateTimePicker"
+              value={chosenDate}
+              mode="time"
+              is24Hour={true}
+              display="default"
+              onChange={onDateChangeHandler}
+            />
+          </View>
+          <PressableButton
+            customizedStyle={styles.button}
+            onPress={handleNotificationSchedule}
+          >
+            <Text>Confirm</Text>
+          </PressableButton>
+          <PressableButton
+            customizedStyle={styles.button}
+            onPress={cancelNotificationTimeHandler}
+          >
+            <Text>Cancel</Text>
+          </PressableButton>
         </View>
-      )}
+      </Modal>
     </View>
   );
 }
+
+const styles = {
+  scheduleButton: {
+    backgroundColor: colors.backgroundColor,
+    borderRadius: 30,
+    height: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 5,
+  },
+  scheduleButtonText: {
+    color: colors.primary,
+    fontSize: 13,
+  },
+  modalContent: {
+    backgroundColor: "white",
+    padding: 22,
+    borderRadius: 10,
+    borderColor: "rgba(0, 0, 0, 0.1)",
+  },
+  button: {
+    backgroundColor: colors.buttonBackground,
+    borderRadius: 5,
+    height: 40,
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+  },
+  dateTimePickerContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 1,
+    padding: 50,
+  },
+};
