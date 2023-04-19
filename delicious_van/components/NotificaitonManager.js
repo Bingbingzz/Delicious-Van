08963b1cb7f02c295d0cac3620b082fc806f3809
Lydia <zhao.ce1@northeastern.edu>
2023-04-18 23:59:53 -0700

    change the style of clock picker to look good in dark mode

--- delicious_van/components/NotificaitonManager.js
+++ delicious_van/components/NotificaitonManager.js
@@ -1,7 +1,6 @@
 import { View, Button, Alert, Text } from "react-native";
 import React, { useState } from "react";
 import * as Notifications from "expo-notifications";
-//import DateTimePicker from "@react-native-community/datetimepicker";
 import PressableButton from "./PressableButton";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import Modal from "react-native-modal";
@@ -10,8 +9,7 @@ import DateTimePickerModal from "react-native-modal-datetime-picker";
 
 export default function NotificationManager() {
   const [selectedDate, setSelectedDate] = useState(new Date());
-  //const [chosenDate, setChosenDate] = useState(new Date());
-  const [isDatePickerVisible, setDatePickerVisibility] = useState(true);
+  const [datePickerVisible, setDatePickerVisibility] = useState(false);
 
   async function verifyPermission() {
     const permissionResponse = await Notifications.getPermissionsAsync();
@@ -46,6 +44,7 @@ export default function NotificationManager() {
           repeats: true,
         },
       });
+      console.log(hour, minute);
       Alert.alert(
         "Notification scheduled at " + selectedDate.toLocaleTimeString([],{timeStyle: 'short'})
       );
@@ -83,8 +82,10 @@ export default function NotificationManager() {
         <Text style={styles.scheduleButtonText}>Schedule a notification</Text>
       </PressableButton>
       <DateTimePickerModal
-        isVisible={isDatePickerVisible}
+        textColor= '#F5793B'
+        isVisible={datePickerVisible}
         mode="time"
+        date={selectedDate}
         onChange={onDateChangeHandler}
         onConfirm={handleConfirm}
         onCancel={hideDatePicker}
